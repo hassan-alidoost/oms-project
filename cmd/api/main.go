cf171838ac65efaa43b12bb752674c96de832daf
hassan-alidoost <h.alidoost76@gmail.com>
2026-05-08 09:47:19 +0330

    feat: wire everything in wire file

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,13 +1,8 @@
 package main
 
 import (
-	"log"
-
-	"github.com/hassan-alidoost/oms-project/config"
 	"github.com/hassan-alidoost/oms-project/internal/app"
 )
-
-const configPath string = "./../../config"
 
 // @title OMS API
 // @version 1.0
@@ -15,11 +10,5 @@ const configPath string = "./../../config"
 // @host localhost:8080
 // @BasePath /
 func main() {
-	cfg, err := config.LoadConfig(configPath)
-
-	if err != nil {
-		log.Fatalf("could not load the config %v", err)
-	}
-
-	app.Initialize(cfg)
+	app.Initialize()
 }
--- internal/app/wire.go
+++ internal/app/wire.go
@@ -14,8 +14,17 @@ import (
 	_ "github.com/hassan-alidoost/oms-project/docs"
 )
 
+const configPath string = "./../../config"
 
-func Initialize(cfg *config.Config) {
+func Initialize() {
+
+	cfg, err := config.LoadConfig(configPath)
+
+	if err != nil {
+		log.Fatalf("could not load the config %v", err)
+	}
+
+
 	fmt.Printf("start running app in %s enviorment", cfg.App.Env)
 	
 	db, cleanup, err := postgres.NewPostgresDB(cfg.Database)
